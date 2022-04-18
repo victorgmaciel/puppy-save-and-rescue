a0c4c92c4a42ae37b75e8b03b91e8a31c11b1964
Alan Gonzalez <alanboy@alanboy.net>
2022-04-18 10:14:15 -0700

    Adding devmatch test cases

---
+++ tests/api.test.js
@@ -0,0 +1,21 @@
+const api = require('../dist/api.js');
+
+test('getPets should return all rows', async () => {
+  let result = await api.getPets();
+  expect(result.statusCode).toBe(200);
+
+  let response = JSON.parse(result.body)
+  expect(response).toHaveLength(3);
+
+  expect(response[0].name).toBe('Bailey');
+});
+
+
+test('getPetsById should return a single pet', async () => {
+  let result = await api.getPetById();
+  expect(result.statusCode).toBe(200);
+});
+
+
+
+
