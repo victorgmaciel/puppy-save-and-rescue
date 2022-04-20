d0f02774193bf1f32b92b3c2ad72b5f7d1e66007
Alan Gonzalez <alanboy@alanboy.net>
2022-04-20 10:05:21 -0700

    finish tests

--- .devmatch/devmatch-validation.test.js
+++ .devmatch/devmatch-validation.test.js
@@ -4,19 +4,32 @@
 const api = require('../dist/api.js');
 
 test('PET_BY_ID', async () => {
-  let result = await api.getPets();
+  let result = await api.getPetById.call(null, { pathParameters: { id: 1 } });
   expect(result.statusCode).toBe(200);
-
   let response = JSON.parse(result.body)
-  expect(response).toHaveLength(3);
+  expect(response.name).toBe('Bailey');
 
-  expect(response[0].name).toBe('Bailey');
+  result = await api.getPetById.call(null, { pathParameters: { id: 2 } });
+  expect(result.statusCode).toBe(200);
+  response = JSON.parse(result.body)
+  expect(response.name).toBe('Miley');
+
+  result = await api.getPetById.call(null, { pathParameters: { id: 3 } });
+  expect(result.statusCode).toBe(200);
+  response = JSON.parse(result.body)
+  expect(response.name).toBe('Chip');
 });
 
 
 test('LOST_PETS', async () => {
-  let result = await api.getPetById();
+  let result = await api.getLostPets();
   expect(result.statusCode).toBe(200);
+
+  let response = JSON.parse(result.body)
+  expect(response.length).toBe(2);
+
+  expect(response[0].name).toBe('Miley');
+  expect(response[1].name).toBe('Chip');
 });
 
 
